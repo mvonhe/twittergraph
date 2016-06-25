262c6018e76cb3e05c569caf40759d2e373b9b79
mvonhe <mvon@ch.ibm.com>
2016-06-25 18:59:47 +0200

    some changes in the py file

--- twitter_test.py
+++ twitter_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import twitter, json, codecs
+import twitter, json, codecs, random
 
 api = twitter.Api(consumer_key="UCQpUai3HNPvVQakSjq17GuKg",
                   consumer_secret="dgf58jmBmjUtTQzE2YQq6NskgefNvwaa2xBRDVhj9W584jODnj",
@@ -13,15 +13,18 @@ name = "Archer_Sterling"
 friends = api.GetFriends(screen_name=name)
 
 def writeJson():
+    cnt=0
     infile = codecs.open('the.json', 'w', 'utf-8')
     infile.write('{\n  "nodes": [\n')
     for fr in friends[:-1]:
-        infile.write('    {"id": "' + fr.name + '", "group": 1},\n')
+        infile.write('    {"id": "' + fr.name + '", "group": '+ str(cnt%5) +'},\n')
+        cnt+=1
     infile.write('    {"id": "' + friends[len(friends)-1].name + '", "group": 1}\n  ],\n"links": [\n')
-
+    cnt = 0
     for fr in friends[:-1]:
-        infile.write('    {"source": "' + name + '", "target": "' + fr.name + '", "value": 3},\n')
-    infile.write('    {"source": "' + name + '", "target": "' + friends[len(friends)-1].name + '", "value": 1}\n  ]\n}')
+        infile.write('    {"source": "' + name + '", "target": "' + fr.name + '", "value": '+str(int(random.randrange(5)))+'},\n')
+        cnt+=1
+    infile.write('    {"source": "' + name + '", "target": "' + friends[len(friends)-1].name + '", "value": '+str(int(random.randrange(5)))+'\n  ]\n}')
     infile.close()
 
 ## testing
